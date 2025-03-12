9e19340f788b8dbed68d70430535dd0661b72e77
fisayo452 <154730960+fisayo452@users.noreply.github.com>
2025-03-12 21:11:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => { const form = document.querySelector('form');
 
-form.addEventListener('submit', (event) => { // If you want to TEST locally without sending data to Netlify, // uncomment the next line: // event.preventDefault();
-
-const username = form.querySelector('input[name="username"]').value;
-const password = form.querySelector('input[name="password"]').value;
-
-// Logs credentials in the browser console (for debugging).
-console.log("Username:", username);
-console.log("Password:", password);
-
-}); });
